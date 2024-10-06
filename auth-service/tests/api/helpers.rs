d18afad005c7ffaaf0119ac1ee2bd2b886bafa9b
JiriMON <moninec@gmail.com>
2024-10-06 12:32:34 +0200

    [sprint 1][task 7] fix: test function names updated to post instead of get,                    add: added test function to test post signup return 422                    add: signup json deserialize funciton

--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -1,4 +1,5 @@
 use auth_service::Application;
+use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
@@ -14,14 +15,17 @@ impl TestApp {
         let address = format!("http://{}", app.address.clone());
 
         // Run the auth service in a separate async task
-        // to avoid blocking the main test thread. 
+        // to avoid blocking the main test thread.
         #[allow(clippy::let_underscore_future)]
         let _ = tokio::spawn(app.run());
 
         let http_client = reqwest::Client::new(); // Create a Reqwest http client instance
 
         // Create new `TestApp` instance and return it
-        TestApp{address,http_client}
+        TestApp {
+            address,
+            http_client,
+        }
     }
 
     pub async fn get_root(&self) -> reqwest::Response {
@@ -32,16 +36,28 @@ impl TestApp {
             .expect("Failed to execute request.")
     }
 
-    //  Implement helper functions for all other routes (signup, login, logout, verify-2fa, and verify-token)
+    /* //  Implement helper functions for all other routes (signup, login, logout, verify-2fa, and verify-token)
     pub async fn get_signup(&self) -> reqwest::Response {
         self.http_client
             .post(&format!("{}/signup", &self.address))
             .send()
             .await
             .expect("Failed to execute request.")
+    } */
+
+    pub async fn post_signup<Body>(&self, body: &Body) -> reqwest::Response
+    where
+        Body: serde::Serialize,
+    {
+        self.http_client
+            .post(&format!("{}/signup", &self.address))
+            .json(body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
     }
-    
-    pub async fn get_login(&self) -> reqwest::Response {
+
+    pub async fn post_login(&self) -> reqwest::Response {
         self.http_client
             .post(&format!("{}/login", &self.address))
             .send()
@@ -49,15 +65,15 @@ impl TestApp {
             .expect("Failed to execute request.")
     }
 
-    pub async fn get_logout(&self) -> reqwest::Response {
+    pub async fn post_logout(&self) -> reqwest::Response {
         self.http_client
             .post(&format!("{}/logout", &self.address))
             .send()
             .await
             .expect("Failed to execute request.")
     }
-    
-    pub async fn get_verify_2fa(&self) -> reqwest::Response {
+
+    pub async fn post_verify_2fa(&self) -> reqwest::Response {
         self.http_client
             .post(&format!("{}/verify-2fa", &self.address))
             .send()
@@ -65,11 +81,15 @@ impl TestApp {
             .expect("Failed to execute request.")
     }
 
-    pub async fn get_verify_token(&self) -> reqwest::Response {
+    pub async fn post_verify_token(&self) -> reqwest::Response {
         self.http_client
             .post(&format!("{}/verify-token", &self.address))
             .send()
             .await
             .expect("Failed to execute request.")
     }
+}
+
+pub fn get_random_email() -> String {
+    format!("{}@example.com", Uuid::new_v4())
 }
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,10 +1,10 @@
 use crate::helpers::TestApp;
 
 #[tokio::test]
-async fn login_returns_auth_ui() {
+async fn login_returns_200() {
     let app = TestApp::new().await;
 
-    let response = app.get_login().await;
+    let response = app.post_login().await;
 
     assert_eq!(response.status().as_u16(), 200);
 }
