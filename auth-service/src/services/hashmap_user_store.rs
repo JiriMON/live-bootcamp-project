4e2f77c999c13504ae82e9a75cc85bd762c0e541
JiriMON <moninec@gmail.com>
2024-10-19 11:48:26 +0200

    [sprint2][task1] add: task completed

---
+++ auth-service/src/services/hashmap_user_store.rs
@@ -0,0 +1,144 @@
+use std::collections::HashMap;
+
+use crate::domain::User;
+
+#[derive(Debug, PartialEq)]
+pub enum UserStoreError {
+    UserAlreadyExists,
+    UserNotFound,
+    InvalidCredentials,
+    UnexpectedError,
+}
+
+// TODO: Create a new struct called `HashmapUserStore` containing a `users` field
+// which stores a `HashMap`` of email `String`s mapped to `User` objects.
+// Derive the `Default` trait for `HashmapUserStore`.
+#[derive(Default)]
+pub struct HashmapUserStore {
+    users: HashMap<String, User>,
+}
+
+impl HashmapUserStore {
+    pub async fn add_user(&mut self, user: User) -> Result<(), UserStoreError> {
+        // Return `UserStoreError::UserAlreadyExists` if the user already exists,
+        // otherwise insert the user into the hashmap and return `Ok(())`.
+        if self.users.contains_key(&user.email) {
+            return Err(UserStoreError::UserAlreadyExists);
+        }
+        self.users.insert(user.email.clone(), user);
+        Ok(())
+    }
+
+    // TODO: Implement a public method called `get_user`, which takes an
+    // immutable reference to self and an email string slice as arguments.
+    // This function should return a `Result` type containing either a
+    // `User` object or a `UserStoreError`.
+    // Return `UserStoreError::UserNotFound` if the user can not be found.
+
+    pub async fn get_user(&self, email: &str) -> Result<User, UserStoreError> {
+        match self.users.get(email) {
+            Some(user) => Ok(user.clone()),
+            None => Err(UserStoreError::UserNotFound),
+        }
+    }
+
+    // TODO: Implement a public method called `validate_user`, which takes an
+    // immutable reference to self, an email string slice, and a password string slice
+    // as arguments. `validate_user` should return a `Result` type containing either a
+    // unit type `()` if the email/password passed in match an existing user, or a `UserStoreError`.
+    // Return `UserStoreError::UserNotFound` if the user can not be found.
+    // Return `UserStoreError::InvalidCredentials` if the password is incorrect.
+    pub async fn validate_user(&self, email: &str, password: &str) -> Result<(), UserStoreError> {
+        match self.users.get(email) {
+            Some(user) => {
+                if user.password.eq(password) {
+                    Ok(())
+                } else {
+                    Err(UserStoreError::InvalidCredentials)
+                }
+            }
+            None => Err(UserStoreError::UserNotFound),
+        }
+    }
+}
+
+// TODO: Add unit tests for your `HashmapUserStore` implementation
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_add_user() {
+        let mut user_store = HashmapUserStore::default();
+        let user = User {
+            email: "test@example.com".to_owned(),
+            password: "password".to_owned(),
+            requires_2fa: false,
+        };
+
+        // Test adding a new user
+        let result = user_store.add_user(user.clone()).await;
+        assert!(result.is_ok());
+
+        // Test adding an existing user
+        let result = user_store.add_user(user).await;
+        assert_eq!(result, Err(UserStoreError::UserAlreadyExists));
+    }
+
+    #[tokio::test]
+    async fn test_get_user() {
+        let mut user_store = HashmapUserStore::default();
+        let email = "test@example.com".to_owned();
+
+        let user = User {
+            email: email.clone(),
+            password: "password".to_owned(),
+            requires_2fa: false,
+        };
+
+        // Test getting a user that exists
+        user_store.users.insert(email.clone(), user.clone());
+        let result = user_store.get_user(&email).await;
+        assert_eq!(result, Ok(user));
+
+        // Test getting a user that doesn't exist
+        let result = user_store
+            .get_user("nonexistent@example.com")
+            .await;
+
+        assert_eq!(result, Err(UserStoreError::UserNotFound));
+    }
+
+    #[tokio::test]
+    async fn test_validate_user() {
+        let mut user_store = HashmapUserStore::default();
+        let email = "test@example.com".to_owned();
+        let password = "password".to_owned();
+
+        let user = User {
+            email: email.clone(),
+            password: password.clone(),
+            requires_2fa: false,
+        };
+
+        // Test validating a user that exists with correct password
+        user_store.users.insert(email.clone(), user.clone());
+        let result = user_store.validate_user(&email, &password).await;
+        assert_eq!(result, Ok(()));
+
+        // Test validating a user that exists with incorrect password
+        let wrong_password = "wrongpassword".to_owned();
+        let result = user_store.validate_user(&email, &wrong_password).await;
+        assert_eq!(result, Err(UserStoreError::InvalidCredentials));
+
+        // Test validating a user that doesn't exist
+        let result = user_store
+            .validate_user(
+                "nonexistent@example.com",
+                &password,
+            )
+            .await;
+
+        assert_eq!(result, Err(UserStoreError::UserNotFound));
+    }
+}
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,5 +1,34 @@
 use crate::helpers::{get_random_email, TestApp};
+use auth_service::routes::SignupResponse;
 
+#[tokio::test]
+async fn should_return_201_if_valid_input() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": true
+    });
+
+    let response = app.post_signup(&signup_body).await;
+    assert_eq!(response.status().as_u16(), 201);
+
+    let expected_response = SignupResponse {
+        message: "User created successfully!".to_owned(),
+    };
+
+    // Assert that we are getting the correct response body!
+   assert_eq!(
+        response
+            .json::<SignupResponse>()
+            .await
+            .expect("Could not deserialize response body to UserBody"),
+        expected_response
+    );
+}
 
 #[tokio::test]
 async fn should_return_422_if_malformed_input() {
